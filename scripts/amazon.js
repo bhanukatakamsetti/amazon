cd919cb0e28e893ae7f521e77266d35cd877b198
bhanukatakamsetti <bhanukatakamsetti246.16@gmail.com>
2025-11-21 15:34:22 +0530

    add cart functionality and integrate scripts for product management

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,10 +46,34 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHtml;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+     button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            let matchingItem;
+
+            cart.forEach((item) => {
+                  if (item.productId === productId) {
+                        matchingItem = productId;
+                  }
+            });
+
+            if( matchingItem ) {
+                  matchingItem.quantity += 1;
+            }
+            else{
+                    cart.push({productId,
+                       quantity: 1
+            })
+            }
+            
+            console.log(cart);
+     })});
